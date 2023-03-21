548cb2fa22c11131812d647de41726f562ecc607
tanipa1 <tania35.291@gmail.com>
2023-03-21 20:13:51 +0600

    props

--- js/countries.js
+++ js/countries.js
@@ -12,12 +12,37 @@ const container = document.getElementById('countries');
 container.innerHTML = countriesHtml.join(' ');
 }
 
-const getCountryHtml = country =>{
+// Original
+/* const getCountryHtml = country =>{
     return`
     <div class="country">
     <h2>${country.name.common}</h2>
     <img src="${country.flags.png}">
     </div>
     `
+} */
+
+// Option-1
+/* const getCountryHtml = country =>{
+    const {name, flags} = country;
+    return`
+    <div class="country">
+    <h2>${name.common}</h2>
+    <img src="${flags.png}">
+    </div>
+    `
+} */
+
+// Option-2
+const getCountryHtml = ({name, flags, area, region}) =>{
+    return`
+    <div class="country">
+    <h2>${name.common}</h2>
+    <p>Area : ${area}</p>
+    <p>Continent : ${area}</p>
+    <img src="${flags.png}">
+    </div>
+    `
 }
+
 loadCountries();
